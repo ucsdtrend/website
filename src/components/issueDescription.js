c18428d25948cfdf6f529ffc2895815e120bcb09
Sydney Wong <syd.wong888@gmail.com>
2023-01-15 15:45:06 -0800

    add featured issue to issues page

--- src/components/issueDescription.js
+++ src/components/issueDescription.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col } from "react-grid-system";
+import { Container, Row, Col } from "react-grid-system";
 import "../styles/IssueDescription.scss";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
@@ -8,19 +8,22 @@ class IssueDescription extends React.Component {
   render () {
     return (
       <div className="issue-container">
-        <Row>
-          <Col md={8}>
-            <a href={this.props.link}>
-              <h3 className="issue-title" >{this.props.title}</h3> 
-            </a>
-            <h4>{this.props.description}</h4>
-          </Col>
-          <Col md={4}>
-            <div className="issue-image-container">
-              <LazyLoadImage className="issue-image" src={this.props.picture} alt={this.props.title} />
-            </div>
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col md={8}>
+              <a href={this.props.link}>
+                <h3 className="issue-title" >{this.props.title}</h3> 
+              </a>
+              <h4>{this.props.description}</h4>
+            </Col>
+            <Col md={4}>
+              <div className="issue-img-container">
+                <LazyLoadImage className="issue-img" src={this.props.picture} alt={this.props.title} />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        
       </div>
     )
   }
--- src/pages/issues.js
+++ src/pages/issues.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-grid-system";
 import Gallery from "../components/issuesGallery";
 import { issuesInfo } from "../components/issuesInfo";
 import IssueBlock from "../components/issueBlock";
-import Button from "../components/button";
+import IssueDescription from "../components/issueDescription";
 import "../styles/Issues.scss";
 import Footer from "../components/footer";
 
@@ -16,10 +16,11 @@ class Issues extends React.Component {
     return (
       <div className="issues-page">
         <Gallery />
+        <IssueDescription picture={issuesInfo[0].picture} title={issuesInfo[0].title} link={issuesInfo[0].link} description={issuesInfo[0].description}/>
         <Container className="issues-grid-container">
           <div className="issues">
           <Row justify="center">
-            {issuesInfo.map((issue, idx) => {
+            {issuesInfo.slice(1).map((issue, idx) => {
               return (
                 <React.Fragment key={issue.title}>
                   <Col xs={6} sm={6} md={5} lg={4} key={idx} >
