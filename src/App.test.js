0dd31d9c334b249fc7c422e1c73d253b56231957
uyenphtran <114267962+uyenphtran@users.noreply.github.com>
2024-01-16 15:56:12 -0800

    updates to website
    Update website title from "React App" to "TREND"
    Update App.test.js
    Minor bug fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App as expected', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appElement = screen.getByTestId("App-element");
+  expect(appElement).toBeInTheDocument();
 });
