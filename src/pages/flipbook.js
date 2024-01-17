2af88ef395a2e7f58485a29fa942b8d18f43c82b
uyenphtran <114267962+uyenphtran@users.noreply.github.com>
2024-01-17 14:46:07 -0800

    Added pictures
    Added pictures to flipbook
    Need to update formatting but in general ready for demo

--- src/pages/flipbook.js
+++ src/pages/flipbook.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { useCallback, useRef } from 'react';
 import HTMLFlipBook from 'react-pageflip';
-import {PageFlip} from 'page-flip';
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import "../styles/Team.scss";
 
 class Flipbook extends React.Component {
@@ -12,8 +9,13 @@ class Flipbook extends React.Component {
     
     render () {
         return (
-            <div width={100} height={100}>
-                <LazyLoadImage width={350} height={500} className="page1" src={require("../static/fb/page1.jpg")} alt={"Page 1"} />
+            <div>
+                <HTMLFlipBook width={300} height={500}>
+                    <img src={require("../static/fb/page1.jpg")}></img>
+                    <img src={require("../static/fb/page2.jpg")}></img>
+                    <img src={require("../static/fb/page3.jpg")}></img>
+                    <img src={require("../static/fb/page4.jpg")}></img>
+                </HTMLFlipBook>
             </div>
         )
     }
