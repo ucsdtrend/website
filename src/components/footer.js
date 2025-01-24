3ce93b7137106d90cd675495f27d68c13985df2e
jayygao <jug015@ucsd.edu>
2025-01-23 17:09:38 -0800

    make blogs

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,30 +11,8 @@ class Footer extends React.Component {
     const medium = "https://medium.com/@trenducsd";
 
     return (
-      <div className="footer-container" id="footer-section">
-          <Container fluid>
-            <Row justify="between">
-              <Col className='footer-name-container'>
-                <h5>© TREND at UC San Diego 2024</h5>
-              </Col>
-              <Col className='footer-logo-container'>
-                  <div className="horizontal-links-container" id="navbar-socials">
-                    <a href={instagram}>
-                      <BsInstagram />
-                    </a>
-                    <a href={facebook}>
-                      <BsFacebook />
-                    </a>
-                    <a href={medium}> 
-                      <FaMedium />
-                    </a>
-                  </div>
-              </Col>
-            </Row>
-            <Row justify="end" className='email-container'>
-              <h5>trenducsd@gmail.com</h5>
-            </Row>
-          </Container>
+      <div className="footer-container">
+        
       </div>
     )
   }
---
+++ src/components/youTrend.js
@@ -0,0 +1,30 @@
+import React from "react";
+import '../styles/youTrend.scss';
+
+class youTrend extends React.Component {
+  render() {
+    return(
+        <div className="youTrend-container">
+            <div className="header">
+              <div className="Trend-logo">
+
+              </div>
+              <p className="keepUp">
+                Keep up with our socials!
+              </p>
+              <img className="ig">
+
+              </img>
+              <img className="meta">
+
+              </img>
+              <img className="medium">
+
+              </img>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default youTrend;
