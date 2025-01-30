cd4b779907ebef341fcf47f8b14e1964b676d7a1
Zander Vilaysane <zandervilaysane@gmail.com>
2025-01-29 19:32:39 -0800

    cleaning

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,8 @@
 import React from "react";
 import FeaturedIssue from "../components/featuredIssue";
 import WhoWeAre from "../components/whoWeAre";
-import WhoWeAreV2 from "../components/WhoWeAreV2";
 import HomepageVideo from "../components/homepageVideo.js"
 import Blogs from "../components/blogs.js"
-import Footer from "../components/footer.js"
-import YouTrend from "../components/youTrend.js"
 
 class Home extends React.Component {
   componentDidMount() {
@@ -19,9 +16,7 @@ class Home extends React.Component {
         <HomepageVideo/>
         <FeaturedIssue/>
         <Blogs/>
-        <WhoWeAreV2/>
-        <YouTrend/>
-        <Footer/>
+        <WhoWeAre/>
         {/* <FeaturedBlog/> */}
       </div>
     )
