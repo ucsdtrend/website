e7aabc7a808e6b23183916b76df0299290b541dd
Angelo Belardo <131566091+Agentfriedrice@users.noreply.github.com>
2026-04-13 10:59:29 -0700

    fixed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Footer from "./components/footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Analytics } from '@vercel/analytics/react';
 
 
 // setConfiguration({
@@ -30,7 +29,6 @@ function App() {
         </Routes>
         <Footer/>
       </BrowserRouter>
-      <Analytics />
     </div>
   );
 }
